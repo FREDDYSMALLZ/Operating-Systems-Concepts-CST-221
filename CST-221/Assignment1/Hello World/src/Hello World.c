85e55cfaef3bb0336afec356a26a639b4b7adbb3
Fredrick Ondieki <freddysmallz@gmail.com>
2019-03-10 19:27:59 -0500

    Commit after adding a copyright

---
+++ CST-221/Assignment1/Hello World/src/Hello World.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Hello.c
+ Author      : Fredrick Ondieki
+ Version     :
+ Copyright   : March 10 2019
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
---
+++ CST-221/Assignment1/test.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+ls
+#List directories
+echo "what is your name?"
+read name
+echo "How do you do, $name?"
+read remark
+echo "I am $remark too!"
+#Environment variables
+whoami
+#Whoami test
+who
+#Who test
+w
+#W test
